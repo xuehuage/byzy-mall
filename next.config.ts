861ffd0073e1fe65a2846b7650172bd2c82a462f
mary <sky@163.com>
2025-11-24 21:43:11 +0800

    feat: 启用生产环境 console.log 自动移除

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: process.env.NODE_ENV === 'production',
-  removeConsole: {
+  compiler: {
+    removeConsole: {
       exclude: ['error'],
     },
+  },
 };
 
 export default nextConfig;
